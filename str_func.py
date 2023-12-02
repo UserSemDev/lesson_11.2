37cf718178732085c05ae2ff853db7e98871067a
Sem <semseveryanin@gmail.com>
2023-12-02 17:26:36 +0300

    added func str_title in str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def str_upper(value):
+    """Функция делает все заглавные буквы в строке"""
     result = value.upper()
+    return result
+
+def str_title(value):
+    """Функция делает все слова с большой буквы в строке"""
+    result = value.title()
     return result
